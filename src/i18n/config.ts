2a21eaaf7cdb2ebb9992876893f946f6da5c98fe
Mohammed Hoque <mohammed.hoque@intelex.com>
2024-02-09 10:32:56 -0600

    Update i18n config, css files

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,8 +2,12 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './locales/en/translations.json';
 import esTranslations from './locales/es/translations.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18next.use(initReactI18next).init({
+i18next.use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+    // lng: 'en', // if you're using a language detector, do not define the lng option
     fallbackLng: 'en',
     resources: {
         en: {
@@ -18,10 +22,13 @@ i18next.use(initReactI18next).init({
         {
             translation: esTranslations
         }
-    }
+    },
+    debug: true
 }, (err, t) => {
     if (err) return console.log('something went wrong loading', err);
     t('key'); // -> same as i18next.t
+}).finally(() => {
+    console.log(`Browser Language: ${i18next.language}`);
 });
 
 export default i18next;
